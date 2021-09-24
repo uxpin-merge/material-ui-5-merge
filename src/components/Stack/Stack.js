eb17649c5ce61957aa8359916d0db5eeb9af4d43
Robert Kirkman <therobertkirkman@gmail.com>
2021-09-24 15:17:26 +0900

    added: Stack and preset

---
+++ src/components/Stack/Stack.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import PropTypes from "prop-types";
+import StackM from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+
+function Stack(props) {
+  return (
+    <StackM>
+      {props.children}
+    </StackM>
+  ) 
+}
+
+Stack.propTypes = {
+  /**
+   * Elements contained by Tooltip.
+   * @uxpinignoreprop 
+   */
+  children: PropTypes.node,
+
+  /**
+   * Defines the flex-direction style property. It is applied for all screen sizes.
+   */
+  direction: PropTypes.oneOf([
+    'column-reverse',
+    'column',
+    'row-reverse',
+    'row',
+    'column-reverse',
+    'column',
+    'row-reverse',
+    'row',
+  ]),
+
+  /**
+   * Add an element between each child.
+   * This needs improving
+   */
+  divider: PropTypes.node,
+
+  /**
+   * Defines the space between immediate children in px
+   */
+  spacing: PropTypes.number,
+}
+
+export default Stack;
