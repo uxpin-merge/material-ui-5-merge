e4728450de2b272181a483bec9fb5b3422d0a1f3
Robert Kirkman <therobertkirkman@gmail.com>
2021-09-24 15:16:44 +0900

    added: Checkbox and preset

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CheckboxM from '@mui/material/Checkbox';
+
+function Checkbox(props) {
+  return <CheckboxM {...props} defaultChecked/>;
+}
+
+Checkbox.propTypes = {
+  /**
+   * If `true`, the component is checked.
+   * @uxpinbind onChange 1
+   */
+  checked: PropTypes.bool,
+
+  /**
+   * @uxpinignoreprop
+   * The icon to display when the component is checked.
+   */
+  checkedIcon: PropTypes.node,
+
+  /**
+   * @uxpinignoreprop
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object,
+
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   */
+  color: PropTypes.oneOf([
+    'default',
+    'primary',
+    'secondary',
+    'error',
+    'success',
+    'warning',
+  ]),
+
+  /**
+   * If `true`, the switch will be disabled.
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * Callback fired when the state is changed.
+   */
+  onChange: PropTypes.func,
+
+  /**
+   * If true, the input element is required.
+   */
+  required: PropTypes.bool,
+
+  /**
+   * The size of the component. small is equivalent to the dense checkbox styling.
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+
+  /**
+   * The value of the component.
+   */
+  value: PropTypes.string
+}
+
+Checkbox.defaultProps = {
+  // NOTE: Checked must be controlled state from the outset, otherwise changing state in the app will trigger an error
+  // see: https://fb.me/react-controlled-components
+  checked: false,
+};
+
+export default Checkbox;
---
+++ src/components/Checkbox/presets/0-default.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Checkbox from '../Checkbox';
+
+export default (
+  <Checkbox uxpId="checkbox-1" />
+)
