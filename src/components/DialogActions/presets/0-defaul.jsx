a468b16d825ac1c45272bc51fbe87f30e1771b09
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-10-12 16:11:08 +0900

    Dialog and DialogActions in progress

---
+++ src/components/DialogActions/presets/0-defaul.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../../Button/Button";
+import DialogActions from "../DialogActions";
+
+export default (
+  <DialogActions uxpId="dialog-actions-1">
+    <Button color="primary" uxpId="button-1">
+      Disagree
+    </Button>
+    <Button color="primary" uxpId="button-2">
+      Agree
+    </Button>
+  </DialogActions>
+);
