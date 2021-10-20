c70bcdff8af594d9e4ee83b0665b3e6d8180f445
Jack Behar <jack@uxpin.com>
2021-10-20 16:59:09 -0600

    CardMedia: added vid, and audio

--- src/components/CardMedia/CardMedia.js
+++ src/components/CardMedia/CardMedia.js
@@ -1,60 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import CardMediaM from '@mui/material/CardMedia';
+import CardMediaM from "@mui/material/CardMedia";
 
 function CardMedia(props) {
-    return (
-        <CardMediaM {...props}>{props.children}</CardMediaM>
-    )
-} 
+  return <CardMediaM controls {...props} />;
+}
 
 CardMedia.propTypes = {
-  /**
-   * Custom Prop.
-   * THe Alt of the Media
-   */
-  alt: PropTypes.string,
-  
-  /**
-   * The Media of the component.
-   * @uxpinignoreprop
-   */
-  children: PropTypes.node,
-
   /**
    * Override or extend the styles applied to the component.
    * @uxpinignoreprop
    */
   classes: PropTypes.object,
-
+  component: PropTypes.oneOf(["img", "video", "audio"]),
   /**
-   * Map typography to another html tag. 
-   */
-  component: PropTypes.string,
-
-  /**
-   * Custom Prop.
-   * The Height of the Media.
-   */
-  height: PropTypes.string,
-
-  /**
-   * Image to be displayed as a background image. 
-   * Either image or src prop must be specified. 
+   * Image to be displayed as a background image.
+   * Either `image` or `src` prop must be specified.
    * Note that caller must specify height otherwise the image will not be visible.
    */
   image: PropTypes.string,
-
   /**
-   * An alias for image property. Available only with media components. Media components: video, audio, picture, iframe, img.
+   * An alias for `image` property.
+   * Available only with media components.
+   * Media components: `video`, `audio`, `picture`, `iframe`, `img`.
    */
   src: PropTypes.string,
-
   /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   * @uxpinignoreprop
-   */
-  sx: PropTypes.object
-}
+ * @uxpinignoreprop
+ */
+  style: PropTypes.object,
+  /**
+The height of the media
+ */
+  height: PropTypes.number
+};
 
-export default CardMedia;
+CardMedia.defaultProps = {
+  component: "img",
+  height: 120,
+  image: "https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
+};
+
+export { CardMedia as default };
