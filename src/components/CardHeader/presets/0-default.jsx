a2d79d37f40bbf91a3fd8f90f879df28c689fe78
Robert Anthony Kirkman <therobertkirkman@gmail.com>
2021-10-20 13:10:49 +0900

    CardHeader: Improvements (#33)
    * CardHeader: Improvements

--- src/components/CardHeader/presets/0-default.jsx
+++ src/components/CardHeader/presets/0-default.jsx
@@ -7,16 +7,9 @@ import IconButton from '../../IconButton/IconButton';
 export default (
   <CardHeader
     uxpId='Card-Header-1'
-    avatar={
-      <Avatar uxpId="Avatar-1" sx={{ bgcolor: 'blue' }}>
-        R
-      </Avatar>
-    }
-    action={
-      <IconButton uxpId="Icon-Button-1" ariaLabel="add accessibility text" >
-        <Icon uxpId="Icon-1">home</Icon>
-      </IconButton>
-    }
+    avatar="MUI"
+    color="red"
+    action="home"
     title="Shrimp and Chorizo Paella"
     subheader="September 14, 2016"
   />
