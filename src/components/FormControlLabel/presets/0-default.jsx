12aefb7f8d0296aec31fde372332b8f521c03238
Jack Behar <jack@uxpin.com>
2021-10-21 15:11:06 -0600

    FormControlLable: change labeel to generic control

--- src/components/FormControlLabel/presets/0-default.jsx
+++ src/components/FormControlLabel/presets/0-default.jsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import FormControlLabel from "../FormControlLabel";
 
 export default (
-  <FormControlLabel label="Radio Label" controlType="radio" uxpId="RadioLabel"/>
+  <FormControlLabel label="Control Label" controlType="radio" uxpId="RadioLabel"/>
 );
