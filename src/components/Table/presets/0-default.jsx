177a0385f6da55ed3abe0090b6c64e9147af6cf1
Jack Behar <jack@uxpin.com>
2021-10-22 15:02:27 -0600

    Table: fixed preset

--- src/components/Table/presets/0-default.jsx
+++ src/components/Table/presets/0-default.jsx
@@ -9,21 +9,21 @@ export default (
   <Table uxpId='Table-1'>
     <TableHead uxpId='Table-Head-1'>
       <TableRow uxpId='Table-Row-0'>
-        <TableCell uxpId="Table-Cell-0-0" align='right'>Header 1</TableCell>
-        <TableCell uxpId="Table-Cell-0-1" align='right'>Header 2</TableCell>
-        <TableCell uxpId="Table-Cell-0-1" align='right'>Header 3</TableCell>
+        <TableCell uxpId="Table-Cell-0-0">Name</TableCell>
+        <TableCell uxpId="Table-Cell-0-1">Username</TableCell>
+        <TableCell uxpId="Table-Cell-0-2">Email</TableCell>
       </TableRow>
     </TableHead>
     <TableBody uxpId='Table-Body-1'>
       <TableRow uxpId='Table-Row-1'>
-        <TableCell uxpId="Table-Cell-1-1">Cell Data 1</TableCell>
-        <TableCell uxpId="Table-Cell-1-2">Cell Data 2</TableCell>
-        <TableCell uxpId="Table-Cell-1-3">Cell Data 3</TableCell>
+        <TableCell uxpId="Table-Cell-1-1">Jack</TableCell>
+        <TableCell uxpId="Table-Cell-1-2">Rob</TableCell>
+        <TableCell uxpId="Table-Cell-1-3">Evan</TableCell>
       </TableRow>
       <TableRow uxpId='Table-Row-2'>
-        <TableCell uxpId="Table-Cell-2-1">Cell Data 1</TableCell>
-        <TableCell uxpId="Table-Cell-2-2">Cell Data 2</TableCell>
-        <TableCell uxpId="Table-Cell-2-3">Cell Data 3</TableCell>
+        <TableCell uxpId="Table-Cell-2-1">jack@email.com</TableCell>
+        <TableCell uxpId="Table-Cell-2-2">robert@email.com</TableCell>
+        <TableCell uxpId="Table-Cell-2-3">evan@email.com</TableCell>
       </TableRow>
     </TableBody>
   </Table>
