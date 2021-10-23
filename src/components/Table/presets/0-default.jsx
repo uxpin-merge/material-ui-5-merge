081991e54a340dcc6d65cfb0d35d810de14c5217
Jack Behar <jack@uxpin.com>
2021-10-22 19:16:06 -0600

    Cleanup

--- src/components/Table/presets/0-default.jsx
+++ src/components/Table/presets/0-default.jsx
@@ -9,20 +9,16 @@ export default (
   <Table uxpId='Table-1'>
     <TableHead uxpId='Table-Head-1'>
       <TableRow uxpId='Table-Row-0'>
-<<<<<<< HEAD
+
         <TableCell uxpId="Table-Cell-0-0">Name</TableCell>
         <TableCell uxpId="Table-Cell-0-1">Username</TableCell>
         <TableCell uxpId="Table-Cell-0-2">Email</TableCell>
-=======
-        <TableCell uxpId="Table-Cell-0-0" align='right'>Header 1</TableCell>
-        <TableCell uxpId="Table-Cell-0-1" align='right'>Header 2</TableCell>
-        <TableCell uxpId="Table-Cell-0-1" align='right'>Header 3</TableCell>
->>>>>>> 23886ac99e49ea235e3131eabf0d95f38e8d7115
+
       </TableRow>
     </TableHead>
     <TableBody uxpId='Table-Body-1'>
       <TableRow uxpId='Table-Row-1'>
-<<<<<<< HEAD
+
         <TableCell uxpId="Table-Cell-1-1">Jack</TableCell>
         <TableCell uxpId="Table-Cell-1-2">Rob</TableCell>
         <TableCell uxpId="Table-Cell-1-3">Evan</TableCell>
@@ -31,16 +27,7 @@ export default (
         <TableCell uxpId="Table-Cell-2-1">jack@email.com</TableCell>
         <TableCell uxpId="Table-Cell-2-2">robert@email.com</TableCell>
         <TableCell uxpId="Table-Cell-2-3">evan@email.com</TableCell>
-=======
-        <TableCell uxpId="Table-Cell-1-1">Cell Data 1</TableCell>
-        <TableCell uxpId="Table-Cell-1-2">Cell Data 2</TableCell>
-        <TableCell uxpId="Table-Cell-1-3">Cell Data 3</TableCell>
-      </TableRow>
-      <TableRow uxpId='Table-Row-2'>
-        <TableCell uxpId="Table-Cell-2-1">Cell Data 1</TableCell>
-        <TableCell uxpId="Table-Cell-2-2">Cell Data 2</TableCell>
-        <TableCell uxpId="Table-Cell-2-3">Cell Data 3</TableCell>
->>>>>>> 23886ac99e49ea235e3131eabf0d95f38e8d7115
+
       </TableRow>
     </TableBody>
   </Table>
