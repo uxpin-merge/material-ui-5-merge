b37b6978f3381eab2df29da95e3e7b52fa845c42
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-12-01 16:42:50 +0900

    created Stepper component

---
+++ src/components/Stepper/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Stepper from '../Stepper'
+
+export default (
+    <Stepper 
+      uxpId='stepper'
+      activeStep={0}
+      orientation='horizontal' 
+      />
+)
