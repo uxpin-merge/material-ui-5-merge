9fe441106ee8a9cdff5f0b7d94dc1572c054e9c3
Jack Behar <jack@uxpin.com>
2022-04-19 16:12:02 -0600

    Changed color

--- src/components/UXPinWrapper/boilerplate-theme.js
+++ src/components/UXPinWrapper/boilerplate-theme.js
@@ -4,7 +4,7 @@ const boilerplateTheme = {
   palette: {
     mode: "light",
     primary: {
-      main: "#1976d2",
+      main: "#000000",
       light: "#42a5f5",
       dark: "#1565c0",
       contrastText: "#ffffff"
